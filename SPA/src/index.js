3138eb6ce5d686018e1c15fbaf8de530d470c31a
ZachAnaz <zachroy04@yahoo.com>
2023-03-13 22:13:03 +0800

    first commit of react projects

---
+++ SPA/src/index.js
@@ -0,0 +1,10 @@
+import REACTDOM from 'react-dom/client';
+function App() {
+    return <h1>Zxch & Anaz</h1>
+}
+const container = document.getElementById('app');
+const root = REACTDOM.createRoot(container);
+root.render(<App />);
+
+
+// console.log("Zxch & Anaz");
